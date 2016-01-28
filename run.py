d7f967804342f1e233ec49f124f29ec73ff714f6
Sam Sheftel <mathias@mailinator.com>
2016-01-29 00:35:33 +0200

    geo

--- run.py
+++ run.py
@@ -11,8 +11,10 @@ from phone import blueprint
 app = Eve()
 
 app.register_blueprint(blueprint, url_prefix='/phone')
-
-
+def pre_contacts_post(resource, request):
+    print(resource)
+    print(request.data)
+app.on_pre_POST += pre_contacts_post
 if __name__ == '__main__':
     host = '0.0.0.0'
     port = int(os.environ.get('PORT')) if 'PORT' in os.environ else 5000
